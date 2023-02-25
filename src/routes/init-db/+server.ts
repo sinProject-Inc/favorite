d1850a933cbfeb0afb024a47b221ebaa28a117e5
Ryan <ryan@websculptors.net>
2023-02-25 16:24:35 +0900

    timeline 実装 #16

--- src/routes/init-db/+server.ts
+++ src/routes/init-db/+server.ts
@@ -16,9 +16,27 @@ export const GET: RequestHandler = async () => {
 		await db.appSettings.create({ data: { key: 'session_lifetime_sec', value: '600' } })
 		await db.appSettings.create({ data: { key: 'pin_code_lifetime_sec', value: '300' } })
 
-		await db.users.create({ data: { email: 'test@test.com', name: 'test_user', role: { connect: { name: Roles.user } } } })
+		await db.users.create({
+			data: { email: 'test@test.com', name: 'test_user', role: { connect: { name: Roles.user } } },
+		})
 
-		await db.posts.create({ data: { detail: 'This is a sample post. #sample #post', user: { connect: { id: 0 } } } })
+		await db.posts.create({
+			data: {
+				created_at: new Date(Date.now()),
+				updated_at: new Date(Date.now()),
+				detail: 'This is a sample post. #sample #post',
+				user: { connect: { id: 1 } },
+			},
+		})
+
+		await db.posts.create({
+			data: {
+				created_at: new Date(Date.now() - 100000),
+				updated_at: new Date(Date.now() - 100000),
+				detail: 'This is another sample post. #sample #post',
+				user: { connect: { id: 1 } },
+			},
+		})
 
 		return new Response('Success')
 	} catch (error) {
